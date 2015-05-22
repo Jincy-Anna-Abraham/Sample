426cfc07a9ea3419aa2c60bc870504d1d6b2f9ea
jincy <jincyabraham94@gmail.com>
2015-05-22 10:24:46 -0700

    added frame aniamtion of background along with sinusoidal motion of bee

--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.sample;
 
+import android.animation.ValueAnimator;
+import android.graphics.Path;
+import android.graphics.PathMeasure;
 import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
@@ -16,17 +20,49 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         initialiseViews();
         setWorld();
-
+        setAnimation();
     }
 
     private void initialiseViews() {
-        frameImageView = (ImageView) findViewById(R.id.imageAnimation);
-        frameImageView.setBackgroundResource(R.drawable.frame_animation_list);
-        frameAnimation = (AnimationDrawable) frameImageView.getBackground();
         imageView = (ImageView)findViewById(R.id.imageView);
     }
 
     private void setWorld() {
+        frameImageView = (ImageView) findViewById(R.id.imageAnimation);
+        frameImageView.setBackgroundResource(R.drawable.frame_animation_list);
+        frameAnimation = (AnimationDrawable) frameImageView.getBackground();
+
+    }
+
+    private void setAnimation() {
+        final Path mPath = new Path();
+        mPath.moveTo(0, 0);
+        mPath.quadTo(50, -50, 100, 225);
+        mPath.quadTo(150, 400, 200, 225);
+        mPath.quadTo(250, -50, 300, 225);
+        mPath.quadTo(350, 400, 400, 225);
+        mPath.quadTo(450, -50, 500, 225);
+        mPath.moveTo(500, 225);
+        ValueAnimator pathAnimator = ValueAnimator.ofFloat(0.0f, 1.0f);
+
+        pathAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            float[] point = new float[2];
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float val = animation.getAnimatedFraction();
+                PathMeasure pathMeasure = new PathMeasure(mPath, true);
+                pathMeasure.getPosTan(pathMeasure.getLength() * val, point, null);
+                imageView.setX(point[0]);
+                imageView.setY(point[1]);
+                imageView.bringToFront();
+                imageView.invalidate();
+            }
+        });
+
+        pathAnimator.setRepeatMode(ValueAnimator.INFINITE);
+        pathAnimator.setDuration(3000);
+        pathAnimator.start();
     }
 
     @Override
@@ -38,4 +74,7 @@ public class MainActivity extends Activity {
             frameAnimation.stop();
         }
     }
+
+
+
 }
